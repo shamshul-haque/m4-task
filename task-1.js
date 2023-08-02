6d2eba91857cc3f83a14c78059615fa38bed2e2b
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-08-02 10:38:05 +0600

    task-1 completed

---
+++ task-1.js
@@ -0,0 +1,20 @@
+/* Harry's mom gave him money asked him to buy some orange and apples. Write a program help harry calculate how much money that shopkeeper will return */
+var momGive = 1000;
+var buyOrange = 320;
+var buyApple = 380;
+var totalCost = 1000 - 320 - 380;
+console.log("Shopkeeper will returns to harry " + totalCost + " tk");
+
+/* Write a program to calculate the average marks of mathematics, Biology, Chemistry, Physics and Bangla of a student */
+var mathematics = 99.5;
+var biology = 75.25;
+var chemistry = 65;
+var physics = 80;
+var bangla = 35.45;
+var avgMark = (99.5 + 75.25 + 65 + 80 + 35.45) / 5;
+console.log(avgMark.toFixed(2));
+
+/* Jhon's teacher gave him two variables. each variable contains a string. Jhon's teacher asked him to combine these two string and print them in one line. Help Jhon to write the program */
+var firstVariable = "I am going to be";
+var secondVariable = "an awesome web developer";
+console.log(firstVariable + " " + secondVariable);
